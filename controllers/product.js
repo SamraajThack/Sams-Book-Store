0ca45dcb3225b71ccf77e011cf54dca4f5f31577
Samraaj Thackeray <samrajig@gmail.com>
2021-04-27 15:11:32 -0400

    Added product model and routes

---
+++ controllers/product.js
@@ -0,0 +1,7 @@
+const Product = require('../models/product');
+const {errorHandler} = require('../helpers/dbErrorHandler');
+
+exports.create = (req, res) => {
+   
+  
+}
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const{ create } = require('../controllers/category');
+const{ create } = require('../controllers/product');
 const{requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const { userById } = require("../controllers/user");
 
 
-router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 
 
 
