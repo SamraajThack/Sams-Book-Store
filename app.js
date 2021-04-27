d6f6d3e89d97c8f9d14553f439c62f38df0f3cb0
Samraaj Thackeray <samrajig@gmail.com>
2021-04-26 21:03:32 -0400

    User signup and error handler

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require("dotenv").config();
 // import routes
-const userRoutes = require('./routes/user');
+const userRoutes = require("./routes/user");
 
 //app
 const app = express();
 
-
 // database
-mongoose.connect(process.env.DATABASE, {
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
-}).then(() => console.log("DB Connected"));
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"));
+
+
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
 
 // routes middleware
-app.use("/api",userRoutes);
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
-
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-}); 
+  console.log(`Server is running on port ${port}`);
+});
