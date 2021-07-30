4e2defd84550e1b8425fc7cb187cbd58dc2df0bc
Samraaj Thackeray <samrajig@gmail.com>
2021-07-30 01:12:45 -0400

    resolved category create bugs - circular imports occuring

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const {errorHandler} = require('../helpers/dbErrorHandler');
 const jwt = require('jsonwebtoken');//to generate signed token
 const expressJwt = require('express-jwt'); //authorization check
 
+
 exports.signup = (req,res)=> {
     console.log("req.body", req.body)
     const user = new User(req.body)
@@ -42,9 +43,9 @@ exports.signin = (req, res) => {
         //generate a signed token with user id and secret
         const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
         //persist the token as 't' in cookie with expiry date
-        res.cookie('t', token, {expire: new Date()+ 9999});
+        res.cookie('t', token, {expire: new Date() + 9999});
         //return response with user and token to frontend client
-        const {_id, name,email, role} = user;
+        const {_id, name, email, role} = user;
         return res.json({token, user: {_id, email, name, role}});
     });
 }
@@ -57,25 +58,24 @@ exports.signout = (req, res) => {
 exports.requireSignin = expressJwt({
     secret: process.env.JWT_SECRET,
     algorithms: ["HS256"],
-    userProperty: "auth"
+    userProperty: 'auth'
 });
 
 exports.isAuth = (req, res, next) => {
-    let user =  req.profile && req.auth && req.profile._id == req.auth._id;
-
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
     if (!user) {
         return res.status(403).json({
-            error: 'Access denied',
+            error: 'Access denied'
         });
     }
     next();
 };
 
 exports.isAdmin = (req, res, next) => {
-    if(req.profile.role === 0){
+    if (req.profile.role === 0) {
         return res.status(403).json({
-            error: 'Admin resource! Access denied'
+            error: 'Admin resourse! Access denied'
         });
     }
     next();
-}
+};
--- routes/category.js
+++ routes/category.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const{ create } = require('../controllers/category');
-const{requireSignin, isAuth, isAdmin} = require('../controllers/auth');
+const { create, categoryById } = require('../controllers/category');
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
 
+//router
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
 
 
-
+//router.param('categoryId', categoryById);
 router.param('userId', userById);
 
 
