7587e2c50c27670f7af0c4759b786a060ac0b9b7
Samraaj Thackeray <samrajig@gmail.com>
2021-07-31 16:52:54 -0400

    Singup form created

--- ecommerce-front/src/user/Signup.js
+++ ecommerce-front/src/user/Signup.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import Layout from '../core/Layout'
-import {API} from '../config';
+
 
 const Signup = () =>  {
+
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input type="text" className='form-control' />
+            </div>
+
+            <div className="form-group">
+                <label  className="text-muted">Email</label>
+                <input type="email" className=" form-control" />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input type="password" className='form-control' />
+            </div>
+
+
+            <div>
+            <button className="btn btn-primary mt-2" >Submit
+            </button>
+            </div>
+
+        </form>
+    )
     return(
-        <Layout title = 'Signup' description = "Signup to E-commerce App">
-            {API}
+        <Layout 
+            title = 'Signup' 
+            description = "Signup to E-commerce App"
+            className = "container col-md-8 offset-md-2"
+            >
+            {signUpForm()}
         </Layout>
-        )
-    }
+        );
+    };
 
 export default Signup
