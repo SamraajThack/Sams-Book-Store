e2a5c6e465caea89ff3ba49b3eca588c6c8583c5
Samraaj Thackeray <samrajig@gmail.com>
2021-08-06 05:41:35 -0400

    Frontend api method for search

--- ecommerce-front/src/core/apiCore.js
+++ ecommerce-front/src/core/apiCore.js
@@ -1,4 +1,6 @@
+import { string } from "prop-types";
 import { API } from "../config.js";
+import queryString from 'query-string';
 
 export const getProducts = (sortBy) => {
   return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
@@ -45,3 +47,17 @@ export const getFilteredProducts = (skip, limit, filters = {}) => {
       console.log(err);
     });
 };
+
+export const list = (params) => {
+    const query = queryString.stringify(params)
+    console.log('query' , query)
+    return fetch(`${API}/products/search?${query}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
