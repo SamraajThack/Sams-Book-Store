9788d1e39ee07e6d9824065dd08123065aee5dbf
Samraaj Thackeray <samrajig@gmail.com>
2021-08-06 09:29:30 -0400

    frontend call to get related products

--- ecommerce-front/src/core/apiCore.js
+++ ecommerce-front/src/core/apiCore.js
@@ -73,3 +73,15 @@ export const getSingleProduct = (productId) => {
       console.log(err);
     });
 };
+
+export const listRelated = (productId) => {
+  return fetch(`${API}/products/related/${productId}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
