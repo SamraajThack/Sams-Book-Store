af82ddb38f1d98bb891e95b2918e92174ac6c9ce
Samraaj Thackeray <samrajig@gmail.com>
2021-08-06 04:54:49 -0400

    Load more button and method added

--- ecommerce-front/src/core/Shop.js
+++ ecommerce-front/src/core/Shop.js
@@ -12,6 +12,7 @@ const Shop = () => {
   const [limit, setLimit] = useState(6);
   const [filteredResults, setFilteredResults] = useState([]);
   const [skip, setSkip] = useState(0);
+  const [size, setSize] = useState(0);
   const [myFilters, setMyFilters] = useState({
     filters: { category: [], price: [] },
   });
@@ -32,8 +33,34 @@ const Shop = () => {
         setError(data.error);
       } else {
         setFilteredResults(data.data);
+        setSize(data.size);
+        setSkip(0);
       }
     });
+  };
+
+  const loadMore = () => {
+    let toSkip = skip + limit;
+    getFilteredProducts(toSkip, limit, myFilters.filters).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults([...filteredResults, ...data.data]);
+        setSize(data.size);
+        setSkip(toSkip);
+      }
+    });
+  };
+
+  const loadMoreButton = () => {
+    return (
+      size > 0 &&
+      size >= limit && (
+        <button onClick = {loadMore} className="btn btn-warning mb-5">
+          Load more
+        </button>
+      )
+    );
   };
 
   useEffect(() => {
@@ -91,13 +118,13 @@ const Shop = () => {
         <div className="col-8">
           <h2 className="mb-4">Products</h2>
           <div className="row">
-              {/* {JSON.stringify({filteredResults})} */}
+            {/* {JSON.stringify({filteredResults})} */}
             {filteredResults.map((product, i) => (
-              
-                <Card key={i}  product={product} />
-              
+              <Card key={i} product={product} />
             ))}
           </div>
+          <hr />
+          {loadMoreButton()}
         </div>
       </div>
     </Layout>
