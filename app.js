fc22bf76fd158286a492a207b32a12a504c788f2
Samraaj Thackeray <samrajig@gmail.com>
2021-08-11 07:30:03 -0400

    Implemented braintree backend setup (routes , controllers, env vars)

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("dotenv").config();
-const expressValidator = require('express-validator');
+const expressValidator = require("express-validator");
 
 // import routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
+const braintreeRoutes = require("./routes/braintree");
 
 //app
 const app = express();
@@ -25,24 +26,23 @@ mongoose
   })
   .then(() => console.log("DB Connected"));
 
-
 //middleware
 app.use(cors()); // app  will be able to handle request coming from different origins
-                // backend and frontend client will have different origins
-                
-app.use(morgan('dev')); //helpful for APIs during development
+// backend and frontend client will have different origins
+
+app.use(morgan("dev")); //helpful for APIs during development
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors()); // app  will be able to handle request coming from different origins
-                // backend and frontend client will have different origins
-
+// backend and frontend client will have different origins
 
 // routes middleware
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
+app.use("/api", braintreeRoutes);
 
 const port = process.env.PORT || 8000;
 
