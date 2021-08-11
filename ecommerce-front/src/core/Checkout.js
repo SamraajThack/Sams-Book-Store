005a5ee896b276d9814a12cd4e3693ef315183a4
Samraaj Thackeray <samrajig@gmail.com>
2021-08-11 10:11:28 -0400

    added braintree drop in and process payments function

--- ecommerce-front/src/core/Checkout.js
+++ ecommerce-front/src/core/Checkout.js
@@ -1,12 +1,38 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
-import { getProducts } from "./apiCore";
+import { getProducts, getBraintreeClientToken, processPayment } from "./apiCore";
 import Card from "./Card";
 import Search from "./Search";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import DropIn from "braintree-web-drop-in-react";
 
 const Checkout = ({ products }) => {
+  const [data, setData] = useState({
+    success: false,
+    clientToken: null,
+    error: "",
+    instance: {},
+    address: "",
+  });
+
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
+  const getToken = (userId, token) => {
+    getBraintreeClientToken(userId, token).then((data) => {
+      if (data.error) {
+        setData({ ...data, error: data.error });
+      } else {
+        setData({clientToken: data.clientToken });
+      }
+    });
+  };
+
+  useEffect(() => {
+    getToken(userId, token);
+  }, []);
+
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
@@ -14,20 +40,91 @@ const Checkout = ({ products }) => {
   };
 
   const showCheckout = () => {
-      return (
-        isAuthenticated() ? (
-            <button className="btn btn-success">Checkout</button>
-          ) : (
-            <Link to="/signin">
-              <button className="btn btn-primary">Sign in to checkout</button>
-            </Link>
-          )
-      )
-  }
+    return isAuthenticated() ? (
+      <div>{showDropIn()}</div>
+    ) : (
+      <Link to="/signin">
+        <button className="btn btn-primary">Sign in to checkout</button>
+      </Link>
+    );
+  };
+
+  const buy = () => {
+    //nonce = data.instance.requestPaymentMethod()
+    //send the nonce to the server
+
+    let nonce;
+    let getNonce = data.instance
+      .requestPaymentMethod()
+      .then((response) => {
+        //console.log(data);
+        nonce = response.nonce;
+        //once you have nonce(card type, card number)
+        //send nonce as 'paymentMethodNonce' to backend
+        //and also total to be charged
+
+        // console.log(
+        //   "send nonce and total to process: ",
+        //   nonce,
+        //   getTotal(products)
+        // );
+
+
+        const paymentData = {
+          paymentMethodNonce: nonce,
+          amount: getTotal(products)
+        }
+        processPayment(userId, token, paymentData)
+        .then(response => {
+          setData({ ...data, success: response.success});
+          //empty cat
+          //create order
+        })
+        .catch(error => console.log(error))
+      })
+      .catch((error) => {
+        //console.log("dropin error: ", error);
+        setData({ ...data, error: error.message });
+      });
+  };
+
+  const showError = (error) => (
+    <div className = "alert alert-danger" style = {{display: error ? '' : 'none'}}>
+      {error}
+       </div>
+  )
+
+  const showSuccess = (success) => (
+    <div className = "alert alert-info" style = {{display: success ? '' : 'none'}}>
+       Thanks! Your payment was successful!
+       </div>
+  )
+
+  const showDropIn = () => {
+    return (
+      <div onBlur = {() => setData({...data, error:""})}>
+        {data.clientToken !== null && products.length > 0 ? (
+          <div>
+            <DropIn
+              options={{
+                authorization: data.clientToken,
+              }}
+              onInstance={(instance) => (data.instance = instance)}
+            />
+            <button onClick={buy} className="btn btn-success col-12">
+              Pay
+            </button>
+          </div>
+        ) : null}
+      </div>
+    );
+  };
 
   return (
     <div>
       <h2>Total: ${getTotal()}</h2>
+      {showSuccess(data.success)}
+      {showError(data.error)}
       {showCheckout()}
     </div>
   );
