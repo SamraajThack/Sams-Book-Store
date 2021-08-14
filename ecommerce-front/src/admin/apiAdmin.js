a517ce782c934e2316a5b2397400636f0d4ae302
Samraaj Thackeray <samrajig@gmail.com>
2021-08-14 14:24:01 -0400

    Manage products component and route added

--- ecommerce-front/src/admin/apiAdmin.js
+++ ecommerce-front/src/admin/apiAdmin.js
@@ -1,4 +1,3 @@
-
 import { API } from "../config.js";
 
 export const createCategory = (userId, token , category) => {
@@ -99,3 +98,72 @@ export const updateOrderStatus = (userId, token, orderId, status) => {
     console.log(err)
   })
 }
+
+
+// to perform crud on products
+// get all products 
+// get a single product
+// update single product
+// delete single product
+
+
+export const getProducts = () => {
+  return fetch(`${API}/products?limit=undefined`, {
+    method: "GET",
+  })
+  .then(response => {
+    return response.json()
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+export const deleteProduct = (productId, userId, token) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": 'application/json',
+      Authorization: `Bearer ${token}`
+    },
+  })
+  .then(response => {
+    return response.json()
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+
+export const getSingleProduct = (productId) => {
+  return fetch(`${API}/product/${productId}`, {
+    method: "GET",
+  })
+  .then(response => {
+    return response.json()
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+
+export const updateProduct = (productId, userId, token, product) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: product
+  })
+  .then(response => {
+    return response.json()
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+   
